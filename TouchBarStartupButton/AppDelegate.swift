4adf12cc752b05b3bd991b36441cb76189eb987e
michelle.zhou2 <michelle.zhou2@workday.com>
2017-07-26 22:22:30 -0700

    Initial Commit

---
+++ TouchBarStartupButton/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TouchBarStartupButton
+//
+//  Created by Michelle Zhou on 7/26/17.
+//  Copyright © 2017 Michelle Zhou. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TouchBarStartupButton/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TouchBarStartupButton
+//
+//  Created by Michelle Zhou on 7/26/17.
+//  Copyright © 2017 Michelle Zhou. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
